98c242a50776244f32bd3f830d59b022e428e78c
basedatosfx-create <basedatosfx@gmail.com>
2026-06-15 16:24:02 -0300

    Update producto.js

--- netlify/functions/producto.js
+++ netlify/functions/producto.js
@@ -1,13 +1,11 @@
 exports.handler = async (event) => {
-  const { img, nombre, codigo, precio } = event.queryStringParameters;
+  const { img } = event.queryStringParameters;
   return {
     statusCode: 200,
     headers: { "Content-Type": "text/html" },
     body: `<!DOCTYPE html>
 <html>
 <head>
-  <meta property="og:title" content="${nombre}">
-  <meta property="og:description" content="Código: ${codigo} | ₲ ${precio}">
   <meta property="og:image" content="${img}">
   <meta property="og:type" content="website">
 </head>
